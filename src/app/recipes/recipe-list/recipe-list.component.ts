875319cad5f41fbe40b29442dc3e138d608f602f
Amanda-Katherine <walker.akw@gmail.com>
2021-03-14 23:58:04 -0400

    import and hard code in a recipes array as starter code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', "A recipe description.", "https://images.freeimages.com/images/large-previews/e7c/recipe-1538714.jpg"),
+    new Recipe('A Test Recipe', "A recipe description.", "https://images.freeimages.com/images/large-previews/e7c/recipe-1538714.jpg")
+
+  ]
+  
 
   constructor() { }
 
