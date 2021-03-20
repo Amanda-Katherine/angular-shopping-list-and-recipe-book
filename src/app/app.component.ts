f3634928f2e83724ca82cb50028d67e1e8fd8aa2
Amanda-Katherine <walker.akw@gmail.com>
2021-03-20 13:01:28 -0400

    create onNavigate method to set loadedFeature value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'angularShoppingListAndRecipesProject';
   loadedFeature = 'recipe'
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature
+  }
 }
