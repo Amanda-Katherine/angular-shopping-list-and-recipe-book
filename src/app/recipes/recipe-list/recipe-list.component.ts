b33873ced34709b2215553cada8ae315c7780b22
Amanda-Katherine <walker.akw@gmail.com>
2021-03-20 15:02:07 -0400

    create new recipeWasSelected EventEmitter and place within onRecipeSelected method (to access in recipes.component)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -8,6 +9,8 @@ import { Recipe } from '../recipe.model'
 })
 
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+  
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', "A recipe description.", "https://images.freeimages.com/images/large-previews/e7c/recipe-1538714.jpg"),
     new Recipe('Another Test Recipe', "Another recipe description.", "https://images.freeimages.com/images/large-previews/e7c/recipe-1538714.jpg")
@@ -18,6 +21,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
