070c8eedd3aec66b9cd8294d8eb6ace024618da5
Amanda-Katherine <walker.akw@gmail.com>
2021-03-20 15:13:42 -0400

    add @ViewChild to access local references for nameInout and amountInout from shopping-edit.comp.html

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Component, EventEmitter, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
 
-  constructor() { }
+  constructor() { }  
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
 
 }
